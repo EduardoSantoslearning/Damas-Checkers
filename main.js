3e7dc1594f0323a6470caea67d003be94ad13754
Eduardo Santos <eduardo_san1999@hotmail.com>
2023-06-23 21:18:27 -0300

    movimentação das peças humano ok

--- main.js
+++ main.js
@@ -1,240 +1,182 @@
-(
-    () => {
-        const tabuleiroDOM = document.querySelector('#tabuleiro')
-        var tabuleiro = [];
-        var tabuleiroJogavel = [];
+const tabuleiroDOM = document.querySelector('#tabuleiro')
+var tabuleiro = [];
+var tabuleiroJogavel = [];
 
-        for (let i = 0; i < 8; i++) {
-            tabuleiroJogavel[i] = [];
+for (let i = 0; i < 8; i++) {
+    tabuleiroJogavel[i] = [];
 
-            for (let j = 0; j < 8; j++) {
+    for (let j = 0; j < 8; j++) {
 
-                tabuleiroJogavel[i][j] = [i, j]
+        tabuleiroJogavel[i][j] = [i, j]
 
-                let quadrado = document.createElement('div')
-                tabuleiroDOM.appendChild(quadrado)
+        var quadrado = document.createElement('div')
+        tabuleiroDOM.appendChild(quadrado)
 
-                if (i % 2 == 0) {
-                    if (j % 2 == 0) {
-                        quadrado.style.backgroundColor = "white";
-                        quadrado.style.color = 'black';
-                    } else {
-                        quadrado.style.backgroundColor = "black";
-                        quadrado.style.color = 'white';
-                    }
-                } else {
-                    if (j % 2 == 0) {
-                        quadrado.style.backgroundColor = "black";
-                        quadrado.style.color = 'white';
-                    } else {
-                        quadrado.style.backgroundColor = "white";
-                        quadrado.style.color = 'black';
-                    }
-                }
-
-                //Distribui peças
-                var value = '1';
-                if (i < 3 && (i + j) % 2 == 1) { // peças máquina
-                    tabuleiroJogavel[i][j][value] = [i, j, 'M']
-                    quadrado.innerHTML = `M`;
-                    quadrado.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][value][2]}')`)
-                } else if (i > 4 && (i + j) % 2 == 1) { // peças humano
-                    tabuleiroJogavel[i][j][value] = [i, j, 'H']
-                    quadrado.innerHTML = `H`;
-                    quadrado.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][value][2]}')`)
-                } else if (i >= 3 && (i + j) % 2 == 1) { // campos disponiveis para movimentação
-                    tabuleiroJogavel[i][j][value] = [i, j, 'P']
-                    quadrado.innerHTML = `0`;
-                    quadrado.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][value][2]}')`)
-                } else { //campos nulos
-                    tabuleiroJogavel[i][j][value] = [i, j, '1']
-
-                }
-
-
-                // adiciona as informações para estilizar e identificar as peças
-                quadrado.setAttribute("id", `${i}${j}`)
-                quadrado.setAttribute("class", `quadrado ${tabuleiroJogavel[i * 8 + j]}`)
-            }
-        }
-
-
-        //Componentes do Jogo
-
-        iniciarJogo(tabuleiroJogavel);
-
-        function iniciarJogo(tabuleiroJogavel) {
-            exibirTabuleiro(tabuleiroJogavel)
-
-            vezHumano = true;
-
-            while (!jogoAcabou(tabuleiroJogavel)) {
-                if (vezHumano) {
-                    realizarJogadaHumano();
-
-                } else {
-                    realizarJogadaMaquina();
-
-                }
-
-                vezHumano = !vezHumano;
-
-                exibirTabuleiro();
-            }
-
-            fim();
-        }
-
-        //selecionar peça
-
-
-
-        function exibirTabuleiro(tabuleiroJogavel) {
-            for (let i = 0; i < tabuleiroJogavel.length; i++) {
-
-            }
-
-        }
-
-        function jogoAcabou(tabuleiroJogavel, value) {
-            pecasHumano = false;
-            pecasMaquina = false;
-            for (let i = 0; i < 8; i++) {
-                for (let j = 0; j < 8; j++) {
-                    console.log(`${tabuleiroJogavel[i][j]}`)
-                    //if ((i + j) % 2 == 1 && tabuleiroJogavel[i][j][value][2] == 'M') {
-                    //    pecasMaquina = true;
-                    //    console.log(pecasHumano, pecasMaquina)
-                    //} else if ((i + j) % 2 == 1 && tabuleiroJogavel[i][j][value][2] == 'H') {
-                    //    pecasHumano = true;
-                    //}
-                }
-
-            }
-            return !pecasHumano || !pecasMaquina;
-
-        }
-
-        function realizarJogadaHumano() {
-
-        }
-
-        function realizarJogadaMaquina() {
-
-        }
-
-
-
-    }
-
-)()
-
-/*
-function iniciarJogo() {
-    inicializarTabuleiro();
-    vezHumano=true;
-    vezMaquina=false;
-
-while(!jogoAcabou()){
-    if (vezHumano){
-        jogadaHumano();
-    }else{
-        jogadaMaquina();
-    }
-    vezHumano=!vezHumano;
-}
-
-
-
-}
-
-function jogoAcabou(){
-
-}
-
-function jogadaHumano(){
-
-}
-*/
-
-function posicionamentosPossiveis(i, j, value) {
-    var x, y;
-    var c = 0; //contador pro array possiveis
-    x = movimenta['selecionada']['i'];
-    y = movimenta['selecionada']['j'];
-
-    document.getElementById(i, j).style.backgroundColor = "#3C9"; //muda cor de fundo
-    possiveis[c] = x + y; c++;
-    //if (tabuleiroJogavel[i][j][value] == )
-
-
-    if (tabuleiroJogavel[i][j][value][2] == 'H') {  //possiveis jogadas humano
-        if (vezHumano = true) {
-
-            if (tabuleiroJogavel[i - 1][j - 1][value][2] == '0') {
-                possivel(i - 1, j - 1);
-            } if (j - 1 > 0 && tabuleiroJogavel[i - 1][j + 1][value][2] == 'M') {
-                possivel(i - 2, j - 2);
-            } if (j + 1 < 8 && peca[i + 1][j + 1][value][2] == 'M') {
-                possivel(i - 2, j + 2);
-            }
-
-            if (i == 7) {
-                if (!tabuleiroJogavel[i - 2][j][value] && !tabuleiroJogavel[i - 1][j][value]) {
-                    possivel(x - 2, y);
-                }
-            }
-
-        }
-
-
-        if (peca[x][y]['cor'] == "preto") {
-
-            if (!peca[x + 1][y]['peca']) {
-                possivel(x + 1, y);
-            } if (y - 1 > 0 && peca[x + 1][y - 1]['peca']) {
-                possivel(x + 1, y - 1);
-            }
-            if (y + 1 < 9 && peca[x + 1][y + 1]['peca']) {
-                possivel(x + 1, y + 1);
-            }
-
-            if (x == 2) {
-
-                if (!peca[x + 2][y]['peca'] && !peca[x + 1][y]['peca']) {
-                    possivel(x + 2, y);
-                }
-
-            }
-
-        }
-    }
-
-    function possivel(px, py) {
-        if (px > 0 && px < 9 && py > 0 && py < 9 && peca[px][py]['cor'] != movimenta['selecionada']['cor']) {
-            document.getElementById('t' + (px) + (py)).style.backgroundColor = "#3C9"; //muda cor de fundo
-            possiveis[c] = "t" + (px) + (py); c++;
-
-            if (!peca[px][py]['peca']) {
-                return true;
+        if (i % 2 == 0) {
+            if (j % 2 == 0) {
+                quadrado.style.backgroundColor = "white";
+                quadrado.style.color = 'black';
+            } else {
+                quadrado.style.backgroundColor = "black";
+                quadrado.style.color = 'white';
             }
         } else {
-            return false;
+            if (j % 2 == 0) {
+                quadrado.style.backgroundColor = "black";
+                quadrado.style.color = 'white';
+            } else {
+                quadrado.style.backgroundColor = "white";
+                quadrado.style.color = 'black';
+            }
+        }
+
+        //Distribui peças
+        if (i < 3 && (i + j) % 2 == 1) { // peças máquina
+            tabuleiroJogavel[i][j] = [i, j, 'M']
+            quadrado.innerHTML = `M`;
+            quadrado.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
+        } else if (i > 4 && (i + j) % 2 == 1) { // peças humano
+            tabuleiroJogavel[i][j] = [i, j, 'H']
+            quadrado.innerHTML = `H`;
+            quadrado.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
+        } else if (i >= 3 && (i + j) % 2 == 1) { // campos disponiveis para movimentação
+            tabuleiroJogavel[i][j] = [i, j, 'P']
+            quadrado.innerHTML = `P`;
+            quadrado.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
+        } else { //campos nulos
+            tabuleiroJogavel[i][j] = [i, j, '1']
+
         }
 
 
+        // adiciona as informações para estilizar e identificar as peças
+        quadrado.setAttribute("id", `${i}${j}`)
+        quadrado.setAttribute("class", `quadrado ${tabuleiroJogavel[i * 8 + j]}`)
+    }
+}
+console.log(tabuleiroJogavel)
+
+
+//Componentes do Jogo
+
+iniciarJogo(tabuleiroJogavel);
+
+function iniciarJogo(tabuleiroJogavel) {
+    exibirTabuleiro(tabuleiroJogavel)
+
+    vezHumano = true;
+
+    while (!jogoAcabou(tabuleiroJogavel)) {
+        if (vezHumano) {
+            realizarJogadaHumano();
+
+        } else {
+            realizarJogadaMaquina();
+
+        }
+
+        vezHumano = !vezHumano;
+
     }
 
-    return c;
 }
 
+function jogoAcabou() {
+
+}
+
+function realizarJogadaHumano() {
+    console.log('Jogada Humano')
+
+
+
+
+}
+
+function realizarJogadaMaquina() {
+    console.log('Jogada Máquina')
+
+}
+
+
+var pecaSelecionada = null;
+var casaDestino = null;
 function seleciona(i, j, value) {
     if (value == 'H') {
-        console.log("acertou")
-        const mudarCor = document.getElementById(`${i}${j}`);
-        mudarCor.style.backgroundColor = 'red';
+
+        if (pecaSelecionada !== null) {
+            const elementoAnterior = document.getElementById(pecaSelecionada);
+            if (elementoAnterior !== null) {
+                elementoAnterior.style.backgroundColor = "black";
+            }
+        }
+
+        const elementoAtual = document.getElementById(`${i}${j}`);
+        if (pecaSelecionada !== `${i}${j}` && elementoAtual !== null) {
+            elementoAtual.style.backgroundColor = "green";
+            pecaSelecionada = `${i}${j}`;
+        } else {
+            pecaSelecionada = null;
+        }
+
+    } else if (value == 'P' && pecaSelecionada > 0) {
+        if (pecaSelecionada - `${i}${j}` == 9 || pecaSelecionada - `${i}${j}` == 11) { //Movimenta a peça
+            tabuleiroJogavel[`${i}`][`${j}`][2] = 'H';  //recebe a peça
+            tabuleiroJogavel[pecaSelecionada.toString().charAt(0)][pecaSelecionada.toString().charAt(1)][2] = 'P'; //desocupa a posição anteiror
+
+
+            console.log(tabuleiroJogavel[pecaSelecionada.toString().charAt(0)][pecaSelecionada.toString().charAt(1)]);
+            console.log(tabuleiroJogavel[`${i}`][`${j}`])
+            //console.log(tabuleiroJogavel[pecaSelecionada[0]])
+
+            pecaSelecionada = null;
+            atualizaTabuleiro();
+            vezMaquina = true;
+
+
+        }
+        //console.log(tabuleiroJogavel)
+
+        //jogadaMaquina();
     }
 
+    //console.log(pecaSelecionada);
+    //return pecaSelecionada;
+}
+
+function realizarJogadaHumano(peca) {
 
 }
 
+function atualizaTabuleiro() {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            console.log(tabuleiroJogavel[i][j][2])
+            let atualiza = document.getElementById(`${i}${j}`)
+
+            //Distribui peças
+            if (tabuleiroJogavel[i][j][2] == 'M') { // peças máquina
+                atualiza.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
+                atualiza.innerHTML = 'M';
+                
+            } else if (tabuleiroJogavel[i][j][2] == 'H') { // peças humano
+                atualiza.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
+                atualiza.innerHTML = 'H';
+                atualiza.style.backgroundColor = "black";
+
+            } else if (tabuleiroJogavel[i][j][2] == 'P') { // campos disponiveis para movimentação
+                atualiza.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
+                atualiza.innerHTML = 'P';
+                atualiza.style.backgroundColor = "black";
+                
+            } else { //campos nulos    
+            }
+
+
+            // adiciona as informações para estilizar e identificar as peças
+            quadrado.setAttribute("id", `${i}${j}`)
+            quadrado.setAttribute("class", `quadrado ${tabuleiroJogavel[i * 8 + j]}`)
+
+        }
+    }
+}
