7b239e62802992d65b0bcc696561607fe7eb187d
Eduardo Santos <eduardo_san1999@hotmail.com>
2023-06-27 14:35:16 -0300

    movimentação e botões completos

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
+//detalhes do tabuleiro
+
+
 const tabuleiroDOM = document.querySelector('#tabuleiro')
 var tabuleiro = [];
 var tabuleiroJogavel = [];
+let inicioDeJogo = null;
+let jogoSalvo = null;
+var contagemPecasHumano = 0;    //contador de pecas capturadas
+var contagemPecasMaquina = 0;   //contador de pecas capturadas
+var pecaSelecionada = null;
+var casaDestino = null;
+
 
 for (let i = 0; i < 8; i++) {
     tabuleiroJogavel[i] = [];
@@ -44,64 +54,29 @@ for (let i = 0; i < 8; i++) {
             quadrado.innerHTML = `P`;
             quadrado.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
         } else { //campos nulos
-            tabuleiroJogavel[i][j] = [i, j, '1']
-
         }
 
 
         // adiciona as informações para estilizar e identificar as peças
         quadrado.setAttribute("id", `${i}${j}`)
         quadrado.setAttribute("class", `quadrado ${tabuleiroJogavel[i * 8 + j]}`)
+
     }
 }
-console.log(tabuleiroJogavel)
+//console.log(tabuleiroJogavel);
+if (tabuleiroJogavel !== null) {
+    inicioDeJogo = JSON.parse(JSON.stringify(tabuleiroJogavel));
+
+    console.log("I", inicioDeJogo);
+    console.log("T", tabuleiroJogavel);
+}
+
+//console.log(tabuleiroJogavel)
 
 
 //Componentes do Jogo
 
-iniciarJogo(tabuleiroJogavel);
-
-function iniciarJogo(tabuleiroJogavel) {
-    exibirTabuleiro(tabuleiroJogavel)
-
-    vezHumano = true;
-
-    while (!jogoAcabou(tabuleiroJogavel)) {
-        if (vezHumano) {
-            realizarJogadaHumano();
-
-        } else {
-            realizarJogadaMaquina();
-
-        }
-
-        vezHumano = !vezHumano;
-
-    }
-
-}
-
-function jogoAcabou() {
-
-}
-
-function realizarJogadaHumano() {
-    console.log('Jogada Humano')
-
-
-
-
-}
-
-function realizarJogadaMaquina() {
-    console.log('Jogada Máquina')
-
-}
-
-
-var pecaSelecionada = null;
-var casaDestino = null;
-function seleciona(i, j, value) {
+function seleciona(i, j, value) {   //selecionar peça e movimentação
     if (value == 'H') {
 
         if (pecaSelecionada !== null) {
@@ -125,58 +100,153 @@ function seleciona(i, j, value) {
             tabuleiroJogavel[pecaSelecionada.toString().charAt(0)][pecaSelecionada.toString().charAt(1)][2] = 'P'; //desocupa a posição anteiror
 
 
-            console.log(tabuleiroJogavel[pecaSelecionada.toString().charAt(0)][pecaSelecionada.toString().charAt(1)]);
-            console.log(tabuleiroJogavel[`${i}`][`${j}`])
-            //console.log(tabuleiroJogavel[pecaSelecionada[0]])
+            pecaSelecionada = null;
+            vezMaquina = true;
+            
+            atualizaTabuleiro();
+            jogadaMaquina();
+
+        } else if (pecaSelecionada - `${i}${j}` == 22 && (pecaSelecionada - `${i}${j}` - 11 == 11 && tabuleiroJogavel[(pecaSelecionada + 11).toString().charAt(0)][(pecaSelecionada + 11).toString().charAt(1)][2] == 'M')) { //come peça do inimigo
+            tabuleiroJogavel[`${i}`][`${j}`][2] = 'H';  //recebe a peça
+
+            tabuleiroJogavel[(pecaSelecionada + 11).toString().charAt(0)][(pecaSelecionada + 11).toString().charAt(1)][2] == 'P'; //desocupa a posição anteiror
+            tabuleiroJogavel[pecaSelecionada.toString().charAt(0)][pecaSelecionada.toString().charAt(1)][2] = 'P'; //desocupa a posição anteiror
 
             pecaSelecionada = null;
+            vezMaquina = true;
+            
             atualizaTabuleiro();
+            jogadaMaquina();
+
+        } else if (pecaSelecionada - `${i}${j}` == 18 && (pecaSelecionada - `${i}${j}` - 9 == 9 && tabuleiroJogavel[(pecaSelecionada + 9).toString().charAt(0)][(pecaSelecionada + 9).toString().charAt(1)][2] == 'M')) { //come peça do inimigo
+            tabuleiroJogavel[`${i}`][`${j}`][2] = 'H';  //recebe a peça
+
+            tabuleiroJogavel[(pecaSelecionada + 11).toString().charAt(0)][(pecaSelecionada + 11).toString().charAt(1)][2] == 'P'; //desocupa a posição anteiror
+            tabuleiroJogavel[pecaSelecionada.toString().charAt(0)][pecaSelecionada.toString().charAt(1)][2] = 'P'; //desocupa a posição anteiror
+
+
+            pecaSelecionada = null;
             vezMaquina = true;
 
+            atualizaTabuleiro();
+            jogadaMaquina();
 
         }
-        //console.log(tabuleiroJogavel)
 
-        //jogadaMaquina();
     }
-
-    //console.log(pecaSelecionada);
-    //return pecaSelecionada;
 }
 
-function realizarJogadaHumano(peca) {
+function jogadaMaquina() {
 
+    if (vezMaquina == true) {
+        vezMaquina = false;
+        atualizaTabuleiro();
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+function notificacao() {
+    if (contagemPecasHumano == 12) {
+        document.getElementById("mensagenNotificacao").innerHTML = "Vitória!"
+    } else if (contagemPecasMaquina == 12) {
+        document.getElementById("mensagenNotificacao").innerHTML = "Derrota!"
+    }
+}
+
+
+
+
+/*===============Game FOOTER================================ */
+
+
+function salvarJogo() {
+    jogoSalvo = JSON.parse(JSON.stringify(tabuleiroJogavel));
+
+    sairJogo();
+}
+
+
+function sairJogo() {
+    tabuleiroJogavel = JSON.parse(JSON.stringify(inicioDeJogo));
+    atualizaTabuleiro();
+    document.getElementById("iniciar").style.display = "flex";
+}
+
+
+
+
+
+
+
+
+
+/*===============Tela Inicial================================ */
+
+function iniciaJogo() {
+    document.getElementById("iniciar").style.display = "none";
+}
+
+function carregarJogo() {
+    document.getElementById("iniciar").style.display = "none";
+    tabuleiroJogavel = JSON.parse(JSON.stringify(jogoSalvo));
+
+    atualizaTabuleiro();
+}
+
+
+
+
+/*===============Atualização das jogadas================================ */
+
+
+
+
 
 function atualizaTabuleiro() {
-    for (let i = 0; i < 8; i++) {
+
+    for (let i = 0; i < 8; i++) { //peças
         for (let j = 0; j < 8; j++) {
-            console.log(tabuleiroJogavel[i][j][2])
             let atualiza = document.getElementById(`${i}${j}`)
 
             //Distribui peças
             if (tabuleiroJogavel[i][j][2] == 'M') { // peças máquina
                 atualiza.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
                 atualiza.innerHTML = 'M';
-                
+                contagemPecasMaquina++;
+
             } else if (tabuleiroJogavel[i][j][2] == 'H') { // peças humano
                 atualiza.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
                 atualiza.innerHTML = 'H';
                 atualiza.style.backgroundColor = "black";
+                contagemPecasHumano++;
 
             } else if (tabuleiroJogavel[i][j][2] == 'P') { // campos disponiveis para movimentação
                 atualiza.setAttribute("onclick", `seleciona(${i}, ${j}, '${tabuleiroJogavel[i][j][2]}')`)
                 atualiza.innerHTML = 'P';
                 atualiza.style.backgroundColor = "black";
-                
+
             } else { //campos nulos    
             }
-
-
-            // adiciona as informações para estilizar e identificar as peças
-            quadrado.setAttribute("id", `${i}${j}`)
-            quadrado.setAttribute("class", `quadrado ${tabuleiroJogavel[i * 8 + j]}`)
-
         }
+    }
+
+    /*===============Game HEADER================================ */
+
+
+    document.getElementById('captJogador2').innerHTML = (12 - `${contagemPecasHumano}`); //atualiza as peças capturadas pelo jogador
+    document.getElementById('captJogador1').innerHTML = (12 - `${contagemPecasMaquina}`); //atualiza as peças capturadas pela máquina
+    contagemPecasHumano = 0;
+    contagemPecasMaquina = 0;
+    if (contagemPecasHumano == 12 || contagemPecasMaquina == 12) {
+        notificacao(contagemPecasHumano, contagemPecasMaquina);
     }
 }
